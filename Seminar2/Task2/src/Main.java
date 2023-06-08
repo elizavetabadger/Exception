595ab9a1d9dc46de7d575b77d2cc1a4cb45eb7e5
unknown <Artbel543@yandex.ru>
2023-06-08 22:24:41 +0300

    HW2_2

---
+++ Seminar2/Task2/src/Main.java
@@ -0,0 +1,23 @@
+/*Если необходимо, исправьте данный код
+(задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+try {
+   int d = 0;
+   double catchedRes1 = intArray[8] / d;
+   System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+   System.out.println("Catching exception: " + e);
+}*/
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try {
+            int d = 1;
+            int intArray[] = {1, 2, 3, 4, 5};
+            double catchedRes1 = intArray[2] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
